25bc60002daf26237d087b8cd6c729d5c101025f
anastasio <Zxl2024903531@outlook.com>
2025-11-23 22:20:27 +0800

    add safe input guardrail, streaming chat model, and chat model listener configuration

--- src/main/java/com/anastasio/aicodehelper/ai/AICodeHelperService.java
+++ src/main/java/com/anastasio/aicodehelper/ai/AICodeHelperService.java
@@ -1,9 +1,15 @@
 package com.anastasio.aicodehelper.ai;
 
+import com.anastasio.aicodehelper.ai.guardrail.SafeInputGuardrail;
+import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.Result;
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.guardrail.InputGuardrails;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
+@InputGuardrails({SafeInputGuardrail.class})
 
 public interface AICodeHelperService {
     @SystemMessage(fromResource = "system-prompt.txt")
@@ -13,4 +19,6 @@ public interface AICodeHelperService {
     @SystemMessage(fromResource = "system-prompt.txt")
     Result<String> chatWithRag(String userMessage);
     record Report(String name, List<String> suggestionList) {}
+    @SystemMessage(fromResource = "system-prompt.txt")
+    Flux<String> streamChat(@MemoryId int memoryId, @UserMessage String userMessage);
 }
--- src/main/java/com/anastasio/aicodehelper/ai/AiCodeHelperServiceFactory.java
+++ src/main/java/com/anastasio/aicodehelper/ai/AiCodeHelperServiceFactory.java
@@ -1,8 +1,11 @@
 package com.anastasio.aicodehelper.ai;
 
+import com.anastasio.aicodehelper.ai.mcp.McpConfig;
+import dev.langchain4j.mcp.McpToolProvider;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.service.AiServices;
 import jakarta.annotation.Resource;
@@ -16,13 +19,21 @@ public class AiCodeHelperServiceFactory {
 
     @Resource
     private ContentRetriever contentRetriever;
+
+    @Resource
+    private StreamingChatModel qwenStreamingChatModel;
+    @Resource
+    private McpToolProvider mcpToolProvider;
     @Bean
     public AICodeHelperService aiCodeHelperService() {
         ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
         AICodeHelperService aiCodeHelperService = AiServices.builder(AICodeHelperService.class)
                 .chatMemory(chatMemory)
                 .chatModel(chatModel)
+                .streamingChatModel(qwenStreamingChatModel)
+                .chatMemoryProvider(memroyId -> MessageWindowChatMemory.withMaxMessages(10))
                 .contentRetriever(contentRetriever)
+                .toolProvider(mcpToolProvider)
                 .build();
         return aiCodeHelperService;
     }
